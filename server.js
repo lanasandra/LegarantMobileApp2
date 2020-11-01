af876f7fe683ff46bba29e17f8d3a76152894e87
lanasandra <sandra.cabessa@gmail.com>
2020-11-01 11:54:06 +0100

    new test connection

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var pg = require('pg');
+
+var app = express();
+
+app.set('port', process.env.PORT || 5000);
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
